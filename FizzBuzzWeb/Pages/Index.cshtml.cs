8d4c6d607b1ed895ab446086649d4e8c3ec69707
Gabriela Czarnowicz <82702@student.pb.edu.pl>
2023-04-05 23:06:12 +0200

    rok

--- FizzBuzzWeb/Pages/Index.cshtml.cs
+++ FizzBuzzWeb/Pages/Index.cshtml.cs
@@ -1,69 +1,74 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using FizzBuzzWeb.Forms;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
 
-namespace FizzBuzzWeb.Pages
+namespace MyApp.Pages
 {
-    public class IndexModel : PageModel
+public class IndexModel : PageModel
+{
+private readonly IHttpContextAccessor _httpContextAccessor;
+private ISession _session => _httpContextAccessor.HttpContext.Session;
+    public IndexModel(IHttpContextAccessor httpContextAccessor)
     {
-        private readonly ILogger<IndexModel> _logger;
-        [BindProperty]
-        public FizzBuzzForm FizzBuzz { set; get; }
-        [BindProperty(SupportsGet = true)]
-        public string Name { get; set; }
-
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
-
-        public void OnGet()
-        {
-            if (string.IsNullOrWhiteSpace(Name))
-            {
-                Name = "Gabrysia";
-
-            }
-        }
-
-        public IActionResult OnPost()
-        {
-            if (!ModelState.IsValid)
-            {
-                return Page();
-            }
-            return RedirectToPage("./Privacy");
-        }
-
-        private void Button1_Click(object sender, EventArgs e)
-        {
-            int number;
-            try
-            {
-                number = Int32.Parse(TextBox1.Text);
-
-                if (number % 3 == 0 && number % 5 == 0)
-                {
-                    MessageBox.Show("FizzBuzz");
-                }
-                else if (number % 3 == 0)
-                {
-                    MessageBox.Show("Fizz");
-                }
-                else if (number % 5 == 0)
-                {
-                    MessageBox.Show("Buzz");
-                }
-                else
-                {
-                    MessageBox.Show("Liczba: " + number + " nie spełnia kryteriów FizzBuzz");
-                }
-            }
-            catch
-            {
-                MessageBox.Show("Wprowadzono nieprawidłowe dane");
-            }
-        }
+        _httpContextAccessor = httpContextAccessor;
     }
 
+    [BindProperty]
+    [Range(1899, 2022, ErrorMessage = "Rok musi być między 1899 a 2022.")]
+    public int Year { get; set; }
+
+    [BindProperty]
+    [StringLength(100, ErrorMessage = "Imię nie może być dłuższe niż 100 znaków.")]
+    public string Name { get; set; }
+
+    public string Result { get; private set; }
+
+    public IActionResult OnPost()
+    {
+        if (!ModelState.IsValid)
+        {
+            return Page();
+        }
+
+        bool isLeap = DateTime.IsLeapYear(Year);
+        Result = $"{Name} urodził{((Name.EndsWith("a") || Name.EndsWith("ę")) ? "a" : "ł")} się w {Year} roku. {(isLeap ? "To był rok przestępny." : "To nie był rok przestępny.")}";
+
+        var results = _session.GetObjectFromJson<List<Result>>("Results") ?? new List<Result>();
+        results.Add(new Result { Name = Name, Year = Year, IsLeap = isLeap });
+        _session.SetObjectAsJson("Results", results);
+            return RedirectToPage("./Saved");
 }
+
+public void OnGet()
+{
+}
+
+public class SavedModel : PageModel
+{
+    private readonly IHttpContextAccessor _httpContextAccessor;
+    private readonly ISession _session;
+    public List<Result> Results { get; set; }
+
+    public SavedModel(IHttpContextAccessor httpContextAccessor)
+    {
+        _httpContextAccessor = httpContextAccessor;
+        _session = _httpContextAccessor.HttpContext.Session;
+    }
+
+    public void OnGet()
+    {
+        Results = _session.GetObjectFromJson<List<Result>>("Results") ?? new List<Result>();
+    }
+}
+
+public class Result
+{
+    public string Name { get; set; }
+    public int Year { get; set; }
+    public bool IsLeap { get; set; }
+}
+
